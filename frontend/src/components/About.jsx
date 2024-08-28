9e40238a01ed884bc8a08e7f69c69a06066df17c
PavishK <kpavish136@gmail.com>
2024-08-28 15:10:05 +0530

    up to dated

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './style.css';
+import Logo from '../assets/aboutus.png';
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="about-container">
+        <div className="about-item">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem exercitationem officia obcaecati et autem a consectetur animi nulla, vel corporis laboriosam doloremque tenetur sequi impedit asperiores dolor voluptatibus! Ducimus, ratione.</p>
+        </div>
+        <div className="about-item">
+          <img src={Logo} alt=''/>
+        </div>
+      </div>
+    </>
   )
 }
 
